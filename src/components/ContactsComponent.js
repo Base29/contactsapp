34329c33ebfecb56756408c538e03e9c877f21e8
Faisal Hussain <faisal@base29.com>
2021-02-24 12:48:32 +0500

    added logic to view phone number of the selected contact

--- src/components/ContactsComponent.js
+++ src/components/ContactsComponent.js
@@ -1,5 +1,5 @@
 //import liraries
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -7,20 +7,26 @@ import {
   ScrollView,
   TouchableOpacity,
   Image,
+  Pressable,
 } from 'react-native';
 
 const placeHolderImage = require('./assets/placeholder.jpg');
 
 // create a component
 const ContactsComponent = (props) => {
-  console.log(props);
+  console.log('Main Props', props);
+  const [contactsVisible, setContactsVisible] = useState(true);
+  const [singleContactVisible, setSingleContactVisible] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState('');
   const renderContact = (contact, key) => {
     return (
       <TouchableOpacity
         style={styles.contactContainer}
         key={key}
         onPress={() => {
-          props.hideContacts();
+          setContactsVisible(false);
+          setSingleContactVisible(true);
+          setPhoneNumber(contact.phoneNumber);
         }}>
         <View style={styles.logoView}>
           <Image
@@ -44,11 +50,34 @@ const ContactsComponent = (props) => {
       </TouchableOpacity>
     );
   };
-  return (
-    <ScrollView>
-      {props.contacts.map((contact, key) => renderContact(contact, key))}
-    </ScrollView>
-  );
+
+  const SingleContact = (props) => {
+    console.log('Single Props', props);
+    return (
+      <View>
+        <Text>{phoneNumber}</Text>
+        <Pressable
+          onPress={() => {
+            setContactsVisible(true);
+          }}>
+          <Text>Choose another</Text>
+        </Pressable>
+      </View>
+    );
+  };
+  if (contactsVisible) {
+    return (
+      <ScrollView>
+        {props.contacts.map((contact, key) => renderContact(contact, key))}
+      </ScrollView>
+    );
+  }
+
+  if (singleContactVisible) {
+    return <SingleContact phoneNumber={phoneNumber} />;
+  }
+
+  return null;
 };
 // define your styles
 const styles = StyleSheet.create({
